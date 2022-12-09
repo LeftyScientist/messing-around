8444505a09a136c421faf8b461e474cf413be6d8
Reno Malanga <renomalanga@gmail.com>
2022-12-09 13:06:22 -0500

    User input info
    Can implement loops for responses for input.

--- doinathing.py
+++ doinathing.py
@@ -1,7 +1,6 @@
 #spec: 1-100, if divisible by 3, return fiz, divisible by 5, return buz, if both, return fizbuz, if neither, return the number
 # modulo returns the remainder
 # while loop to generate returns from 1 to 100
-
 number = 1
 
 while number < 101:
@@ -15,3 +14,17 @@ while number < 101:
     else:
         print(number)
     number += 1
+
+name = input("Hello! My name is ######, what's yours?: ")
+print(name + 'is a lovely name.')
+
+play = input("Would you like to play a math game? [Y/N]: ")
+play = str.upper(play)
+
+if play == 'N':
+    print("Ok, goodbye!")
+elif play == 'Y':
+    print("Great!")
+else:
+    print(input("Type Y or N into the terminal. "))
+
